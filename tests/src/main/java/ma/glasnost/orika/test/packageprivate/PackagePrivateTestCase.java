ffc5900938eecb17ec2660f991012ff6b9186df2
Joachim <mereel@gmx.de>
2017-02-05 20:37:33 +0100

    enable mapping package private nested classes

--- tests/src/main/java/ma/glasnost/orika/test/packageprivate/PackagePrivateTestCase.java
+++ tests/src/main/java/ma/glasnost/orika/test/packageprivate/PackagePrivateTestCase.java
@@ -40,6 +40,28 @@ public class PackagePrivateTestCase {
         assertEquals(source.getField(), actual.getField());
     }
 
+    @Test
+    public void testPackagePrivateNestedEntities() {
+        NestedEntity source = new NestedEntity();
+        source.setField("test value");
+        
+        final NestedEntity actual = getMapperFacade().map(source, NestedEntity.class);
+        
+        assertEquals(source.getField(), actual.getField());
+    }
+    
+    static class NestedEntity {
+        private String field;
+        
+        public String getField() {
+            return field;
+        }
+        
+        public void setField(String field) {
+            this.field = field;
+        }
+    }
+    
     private MapperFacade getMapperFacade() {
         final MapperFactory mapperFactory = MappingUtil.getMapperFactory(true);
         mapperFactory.classMap(SomePrivateEntity.class, SomePublicDto.class);
